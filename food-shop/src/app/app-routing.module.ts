894a4aee4eeffd641808226a0900a3333c3f4865
nguyenphuc355 <nguyenvanphuca9999@gmail.com>
2022-12-01 09:08:34 +0700

    up

--- food-shop/src/app/app-routing.module.ts
+++ food-shop/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeModule} from './home/home.module';
+import {LoginModule} from './login/login.module';
 
 
 const routes: Routes = [
   {path: '', loadChildren: () => HomeModule},
+  {path: 'login', loadChildren: () => LoginModule}
 ];
 
 @NgModule({
